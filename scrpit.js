bf28e1aa9cdbb965d7e9fab90d198d5b5ee3dbea
thailinewalendorff <147395777+thailinewalendorff@users.noreply.github.com>
2025-03-28 10:29:03 -0300

    .

--- scrpit.js
+++ scrpit.js
@@ -11,6 +11,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
+    const alternaContraste = document.getElementById('alterna-contraste')
+
     let tamanhoAtualFonte = 1;
 
     aumentaFonteBotao.addEventListener('click', function () {
@@ -24,4 +26,10 @@ document.addEventListener('DOMContentLoaded', function () {
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
     })
+
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
+    })
+
+
 })
