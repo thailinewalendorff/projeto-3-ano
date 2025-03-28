64d14d49ab8b69589987bf7e1232aed5ee7ee6ee
thailinewalendorff <147395777+thailinewalendorff@users.noreply.github.com>
2025-03-28 10:36:15 -0300

    .

--- scrpit.js
+++ scrpit.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
 
-    })
+    });
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
@@ -19,17 +19,17 @@ document.addEventListener('DOMContentLoaded', function () {
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
-    })
+    });
 
     diminuiFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
-    })
+    });
 
     alternaContraste.addEventListener('click', function () {
         document.body.classList.toggle('alto-contraste')
-    })
+    });
 
 
-})
+});
